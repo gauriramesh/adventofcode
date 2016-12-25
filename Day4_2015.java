cd212da139c5cddc1f82e63dc7dd0c4f9b62fac4
Gauri Ramesh <gauri.ramesh15@gmail.com>
2016-12-24 18:53:59 -0600

    Add files via upload

---
+++ Day4_2015.java
@@ -0,0 +1,53 @@
+/**
+ * Gauri Ramesh
+ * Advent of Code 2015, Day 4, Problem 1
+ * 12/23/2016
+ */
+
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Day4_2015 {
+
+	public static void main(String args[]) {
+		String input = "iwrupvqb";
+		Integer number = 0;
+		int numZeroes = 0;
+		Loop: while (numZeroes != 5) {
+			String numAppend = number.toString();
+			String testInput = input.concat(numAppend);
+			numZeroes = 0;
+			try {
+				// This MessageDigest takes an algorithm as a parameter.
+				MessageDigest md = MessageDigest.getInstance("MD5");
+				md.reset();
+				md.update(testInput.getBytes());
+				// Use a BigInteger when there may be overflow.
+				BigInteger integer = new BigInteger(1, md.digest());
+				String hash = String.format("%1$032X", integer);
+				System.out.println(hash);
+				for (int i = 0; i < 5; i++) {
+					if (hash.charAt(i) == '0') {
+						numZeroes++;
+					}
+					if (numZeroes == 5) {
+						System.out.println(numAppend);
+						break Loop;
+					}
+				}
+				
+				number++;
+				md.reset();
+
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+			// MessageDigest returns a byte array.
+
+		}
+
+	}
+
+}
